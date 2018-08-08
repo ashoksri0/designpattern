81ae3910924197ec521263ec54aeb08f026e9acf
Ashok Kumar <aknatrajan@outlook.com>
2018-08-08 18:38:16 +0530

    builder pattern implemented

--- src/main/java/com/creational/builder/Pizza.java
+++ src/main/java/com/creational/builder/Pizza.java
@@ -2,56 +2,14 @@ package com.creational.builder;
 
 import java.util.Objects;
 
-public final class Pizza
-{
+public final class Pizza {
     private String dough;
     private String sauce;
     private String topping;
     private PIZZA_TYPE type;
-    public enum PIZZA_TYPE{VEG,NON_VEG}
 
-  
-    public static class PizzaBuilder
-    {
-        private String dough;
-        private String sauce;
-        private String topping;
-        private PIZZA_TYPE type;
-        public PizzaBuilder() {
-        }
-        public String getDough() {
-            return dough;
-        }
-
-        public PizzaBuilder setDough(final String dough) {
-            this.dough = dough;
-            return this;
-        }
- 
-
-        public PizzaBuilder setSauce(final String sauce) {
-            this.sauce = sauce;
-            return this;
-        }
- 
-
-        public PizzaBuilder setTopping(final String topping) {
-            this.topping = topping;
-            return this;
-        } 
-
-        public PizzaBuilder setType(final PIZZA_TYPE type) {
-            this.type = type;
-            return this;
-        }
-        
-        public Pizza build()
-        {
-            return new Pizza(dough, sauce, topping, type);
-        }
-    }
     private Pizza(final String dough, final String sauce, final String topping,
-               final PIZZA_TYPE type) {
+                  final PIZZA_TYPE type) {
         this.dough = dough;
         this.sauce = sauce;
         this.topping = topping;
@@ -82,5 +40,47 @@ public final class Pizza
                 ", topping='" + topping + '\'' +
                 ", type='" + type + '\'' +
                 '}';
+    }
+
+    public enum PIZZA_TYPE {VEG, NON_VEG}
+
+    public static class PizzaBuilder {
+        private String dough;
+        private String sauce;
+        private String topping;
+        private PIZZA_TYPE type;
+
+        public PizzaBuilder() {
+        }
+
+        public String getDough() {
+            return dough;
+        }
+
+        public PizzaBuilder setDough(final String dough) {
+            this.dough = dough;
+            return this;
+        }
+
+
+        public PizzaBuilder setSauce(final String sauce) {
+            this.sauce = sauce;
+            return this;
+        }
+
+
+        public PizzaBuilder setTopping(final String topping) {
+            this.topping = topping;
+            return this;
+        }
+
+        public PizzaBuilder setType(final PIZZA_TYPE type) {
+            this.type = type;
+            return this;
+        }
+
+        public Pizza build() {
+            return new Pizza(dough, sauce, topping, type);
+        }
     }
 }
--- src/main/java/com/creational/builder/PizzaExample.java
+++ src/main/java/com/creational/builder/PizzaExample.java
@@ -1,10 +1,9 @@
 package com.creational.builder;
 
-public class PizzaExample
-{
+public class PizzaExample {
     public static void main(String[] args) {
-        Pizza pizza=new Pizza.PizzaBuilder().
-        setDough("cross")
+        Pizza pizza = new Pizza.PizzaBuilder().
+                setDough("cross")
                 .setSauce("mild")
                 .setTopping("pepperoni,salami")
                 .setType(Pizza.PIZZA_TYPE.VEG)

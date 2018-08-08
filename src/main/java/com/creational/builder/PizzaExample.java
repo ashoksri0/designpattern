3b30895a4d106779b2988144b6bceb650178193d
Ashok Kumar <aknatrajan@outlook.com>
2018-08-08 19:10:17 +0530

    builder pattern implemented

--- src/main/java/com/creational/builder/PizzaExample.java
+++ src/main/java/com/creational/builder/PizzaExample.java
@@ -4,12 +4,16 @@ import com.creational.Pizza;
 
 public class PizzaExample {
     public static void main(String[] args) {
-        Pizza pizza = new Pizza.PizzaBuilder().
+       // Pizza pizzaBefore=new Pizza("cross","mild","pepperoni,salami",Pizza.PIZZA_TYPE.VEG);
+        
+        
+        //this is with deign pattern
+        Pizza pizzaAfter = new Pizza.PizzaBuilder().
                 setDough("cross")
                 .setSauce("mild")
                 .setTopping("pepperoni,salami")
                 .setType(Pizza.PIZZA_TYPE.VEG)
                 .build();
-        System.out.println(pizza);
+        System.out.println(pizzaAfter);
     }
 }
